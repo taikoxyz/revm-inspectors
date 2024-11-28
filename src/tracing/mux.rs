aeb5c8e0085bcd6ed64d90921745568cd32b3579
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-11-28 14:53:37 +0100

    fix match

--- src/tracing/mux.rs
+++ src/tracing/mux.rs
@@ -260,6 +260,7 @@ impl DelegatingInspector {
 
                 Ok(DelegatingInspector::Mux(MuxInspector::try_from_config(config)?))
             }
+            _ => unimplemented!(),
         };
 
         inspector.map(|inspector| (tracer_type, inspector))
